b3621a5aaddfc29d74aef3c28ce0925e3e4c9b0c
maphapthieunu <npl@duck.com>
2025-01-07 13:04:20 +0700

    patches/[nat] add click-to-focus to fancybar (found on google).diff

--- dwm.c
+++ dwm.c
@@ -123,6 +123,7 @@ struct Client {
 	int oldx, oldy, oldw, oldh;
 	int basew, baseh, incw, inch, maxw, maxh, minw, minh, hintsvalid;
 	int bw, oldbw;
+	int barx, barw;
 	unsigned int tags;
 	int isfixed, isfloating, isurgent, neverfocus, oldstate, isfullscreen;
 	Client *next;
@@ -208,6 +209,7 @@ static void focusin(XEvent *e);
 static void focusmon(const Arg *arg);
 static void focusnext(const Arg *arg);
 static void focusstack(const Arg *arg);
+static void focustitle(const Arg *arg);
 static Atom getatomprop(Client *c, Atom prop);
 static int getrootptr(int *x, int *y);
 static long getstate(Window w);
@@ -576,8 +578,12 @@ buttonpress(XEvent *e)
 			click = ClkLtSymbol;
 		else if (ev->x > selmon->ww - (int)TEXTW(stext) - getsystraywidth())
 			click = ClkStatusText;
-		else
+		else {
 			click = ClkWinTitle;
+			for (c = m->clients; c; c = c->next)
+				if (c->barx >= 0 && ev->x >= c->barx && ev->x < c->barx + c->barw)
+					arg.v = c;
+		}
 	} else if ((c = wintoclient(ev->window))) {
 		if (focusonwheel || (ev->button != Button4 && ev->button != Button5))
 			focus(c);
@@ -587,7 +593,7 @@ buttonpress(XEvent *e)
 	for (i = 0; i < LENGTH(buttons); i++)
 		if (click == buttons[i].click && buttons[i].func && buttons[i].button == ev->button
 		&& CLEANMASK(buttons[i].mask) == CLEANMASK(ev->state))
-			buttons[i].func(click == ClkTagBar && buttons[i].arg.i == 0 ? &arg : &buttons[i].arg);
+			buttons[i].func((click == ClkTagBar || click == ClkWinTitle) && buttons[i].arg.i == 0 ? &arg : &buttons[i].arg);
 }
 
 void
@@ -952,9 +958,14 @@ drawbar(Monitor *m)
 				mw += ew / i;
 
 			for (c = m->clients; c; c = c->next) {
-				if (!ISVISIBLE(c))
+				if (!ISVISIBLE(c)) {
+					c->barx = c->barw = -1;
 					continue;
+				}
+
 				tw = MIN(m->sel == c ? w : mw, TEXTW(c->name));
+				c->barx = x;
+				c->barw = tw;
 
 				drw_setscheme(drw, scheme[m == selmon && m->sel == c ? SchemeSel : SchemeNorm]);
 				if (tw > lrpad / 2)
@@ -1092,6 +1103,13 @@ focusstack(const Arg *arg)
 		focus(c);
 		restack(selmon);
 	}
+}
+
+void
+focustitle(const Arg *arg)
+{
+	focus((Client *)arg->v);
+	restack(selmon);
 }
 
 Atom
@@ -1326,6 +1344,7 @@ manage(Window w, XWindowAttributes *wa)
 	c->w = c->oldw = wa->width;
 	c->h = c->oldh = wa->height;
 	c->oldbw = wa->border_width;
+	c->barx = c->barw = -1;
 
 	updatetitle(c);
 	if (XGetTransientForHint(dpy, w, &trans) && (t = wintoclient(trans))) {
